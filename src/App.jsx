876e61c888062d9f1ed59597e1946d119bb971bc
Ranjithdnv <rith8596@gmail.com>
2023-09-21 12:23:05 +0530

    h

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <a href="tel:9390083894">call me</a>
+    </div>
+  );
+}
+
+export default App;
